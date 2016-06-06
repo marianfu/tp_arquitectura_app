3a301093e28164b5ef1cefffa49fadb5e813baaa
Calace <alvaro.daniel.calace@accenture.com>
2016-06-06 14:28:01 -0300

    css fix

--- public/js/Benchmarker.js
+++ public/js/Benchmarker.js
@@ -2,24 +2,25 @@
 	function Benchmarker(id) {
 		this.el = document.getElementById(id);
 		this.table = this.el.getElementsByTagName('tbody')[0];
-		this.packetsSpan = this.el.getElementsByClassName('packets-per-second')[0];
-		this.ellapsedSpan = this.el.getElementsByClassName('average-ellapsed-ms')[0];
+		this.packetsPerSecondSpan = this.el.getElementsByClassName('packets-per-second')[0];
+		this.ellapsedMsSpan = this.el.getElementsByClassName('average-ellapsed-ms')[0];
+		this.packetsCountSpan = this.el.getElementsByClassName('packets-count')[0];
 		
 		this.packetCount = 0;
-		this.firstPacketArrivedAt = null;
+		this.firstPacketArrivedAt = 0;
 		this.averageEllapsedMs = 0;
 		
 		this.maxRows = 100;
 	}
 	
 	Benchmarker.prototype.add = function(desc, time, includeEllapsedMs) {
-		this.packetCount++;
+		this.packetsCountSpan.innerText = ++this.packetCount;
 		var now = Date.now();
 		
 		if (this.packetCount === 1) {
 			this.firstPacketArrivedAt = time;
 		} else {
-			this.packetsSpan.innerText = this.packetCount / ((now - this.firstPacketArrivedAt) / 1000);
+			this.packetsPerSecondSpan.innerText = this.packetCount / ((now - this.firstPacketArrivedAt) / 1000);
 		}
 		
 		var row = this.table.insertRow(0);
@@ -30,7 +31,7 @@
 			var ellapsedMs = now - time;
 			row.insertCell(2).innerText = ellapsedMs;
 			this.averageEllapsedMs = this.averageEllapsedMs + (ellapsedMs - this.averageEllapsedMs) / this.packetCount;
-			this.ellapsedSpan.innerText = this.averageEllapsedMs;
+			this.ellapsedMsSpan.innerText = this.averageEllapsedMs;
 		}
 		
 		var rowCount = this.table.rows.length;
